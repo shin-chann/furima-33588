309bb8b42a91273197f8d97167942c84c6aee0fa
shinpei-nakayama <wtpmjgd.a@icloud.com>
2021-02-01 17:00:06 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-
+  before_action :move_to_index, only: [:edit, :update]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -22,10 +22,23 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:product_name, :explanation, :category_id, :state_id, :burden_id, :area_id, :number_of_time_id,
                                  :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to root_path unless user_signed_in? && current_user.id == @item.user_id
   end
 end
