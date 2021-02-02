619627307ee10806be111e3362019bca6fbce04b
shinpei-nakayama <wtpmjgd.a@icloud.com>
2021-02-02 11:29:46 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update]
-  before_action :set_item, only:[:edit, :update, :show]
+  before_action :set_item, only:[:edit, :update, :show, :destroy]
 
-  before_action :move_to_index , only:[:edit, :update]
+  before_action :move_to_index , only:[:edit, :update,]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -32,6 +32,11 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
